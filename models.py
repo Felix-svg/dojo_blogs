a1ebd64ba5613dd2ebaccfb3bb671a31ba2f75f5
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-05-04 22:48:46 +0300

    Update user authentication

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from config import app, db, bcrypt
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import validates
 
 
 class User(db.Model, SerializerMixin):
@@ -20,15 +21,23 @@ class User(db.Model, SerializerMixin):
 
     @hybrid_property
     def password_hash(self):
-        return self._password_hash
+        raise AttributeError('Password hashes may not be viewed.')
 
     @password_hash.setter
     def password_hash(self, password):
-        password_hash = bcrypt.generate_password_hash(password.encode("utf-8"))
-        self._password_hash = password_hash.decode("utf-8")
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
 
-    def authenticate(self):
-        return bcrypt.check_password_hash(self._password_hash, password.encode("utf-8"))
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+
+    @validates("email")
+    def validate_email(self, key, email):
+        if "@" not in email:
+            raise ValueError("Email must include @")
+        return email
 
     def __repr__(self):
         return f"<Name: {self.first_name} {self.last_name}, Email: {self.email}>"
